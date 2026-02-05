20532f1194115bf67a344fd210d79760347283e6
hxuvsck <hxuvsck@gmail.com>
2026-02-06 07:27:26 +0800

    Added DSCEngine.sol as initiative

---
+++ src/DSCEngine.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+import {} from "";
+
+contract DSCEngine {}
+
