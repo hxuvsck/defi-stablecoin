02a31c549aacbe0afeda4e1becacc3a63246d2eb
hxuvsck <hxuvsck@gmail.com>
2026-02-16 17:03:07 +0800

    Deploy script is done

--- script/DeployBiotainStableCoin.s.sol
+++ script/DeployBiotainStableCoin.s.sol
@@ -4,12 +4,25 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {BiotainStableCoin} from "../src/BiotainStableCoin.sol";
 import {BIOTAINEngine} from "../src/BIOTAINEngine.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployBiotainStableCoin is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
     function run() external returns (BiotainStableCoin, BIOTAINEngine) {
-        vm.startBroadcast();
+        HelperConfig config = new HelperConfig();
+
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
+            helperConfig.activeNetworkConfig();
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        vm.startBroadcast(deployerKey);
         BiotainStableCoin bsc = new BiotainStableCoin();
-        // BIOTAINEngine engine = new BIOTAINEngine();
+        BIOTAINEngine engine = new BIOTAINEngine(tokenAddresses, priceFeedAddresses, address(bsc));
+
+        bsc.transferOwnership(address(engine))
         vm.stopBroadcast();
         return (bsc, engine);
     }
