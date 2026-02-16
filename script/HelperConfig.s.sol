1c66502543bf1dadc6e50d41ccb256634f3aeecd
hxuvsck <hxuvsck@gmail.com>
2026-02-16 16:17:00 +0800

    Added constants for mock and started pricefeed of eth broadcasting in anvil

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract HelperConfig is Script {
     struct NetworkConfig {
@@ -13,6 +14,10 @@ contract HelperConfig is Script {
         address wbtc;
         uint256 deployerKey;
     }
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant ETH_USD_PRICE = 2000e8;
+    int256 public constant BTC_USD_PRICE = 1000e8;
 
     NetworkConfig public activeNetworkConfig;
 
@@ -33,5 +38,10 @@ contract HelperConfig is Script {
         if (activeNetworkConfig.wethUsdPriceFeed != address(0)) {
             return activeNetworkConfig;
         }
+
+        vm.startBroadcast();
+        MockV3Aggregator ethUsdPriceFeed = new MockV3Aggregator(DECIMALS, ETH_USD_PRICE);
+
+        vm.stopBroadcast();
     }
 }
