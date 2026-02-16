524d84b8d9cbf5115de4576cff6ede25b259fcdf
hxuvsck <hxuvsck@gmail.com>
2026-02-16 16:09:12 +0800

    Making anvil eth config now

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -12,4 +12,25 @@ contract HelperConfig is Script {
         address wbtc;
         uint256 deployerKey;
     }
+
+    NetworkConfig public activeNetworkConfig;
+
+    constructor() {}
+
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
+        return NetworkConfig({
+            wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306,
+            wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
+            weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
+            wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
+            deployerKey: vm.envUint("PRIVATE_KEY")
+        });
+    }
+
+    // Mock attributes will be needed
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.wethUsdPriceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+    }
 }
