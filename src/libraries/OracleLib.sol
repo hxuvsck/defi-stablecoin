893b36f04f3f4378565b0a30773ade8071734c19
hxuvsck <hxuvsck@gmail.com>
2026-04-08 14:04:23 +0800

    OracleLib functions

--- src/libraries/OracleLib.sol
+++ src/libraries/OracleLib.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 /**
  * @title OracleLib
  * @author Khuslen Ganbat
@@ -13,4 +15,22 @@ pragma solidity ^0.8.18;
  *
  */
 
-library OracleLib {}
+library OracleLib {
+    error OracleLib__StalePrice();
+
+    // hardcoded heartbeats except Chainlink dynamic per heartbeat to pairs
+    uint256 private constant TIMEOUT = 3 hours; // 10800 seconds
+
+    function stalePriceCheck(AggregatorV3Interface priceFeed)
+        public
+        view
+        returns (uint80, int256, uint256, uint256, uint80)
+    {
+        (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) =
+            priceFeed.latestRoundData();
+
+        uint256 secondsSince = block.timestamp - updatedAt;
+        if (secondsSince > TIMEOUT) revert OracleLib__StalePrice();
+        return (roundId, answer, startedAt, updatedAt, answeredInRound);
+    }
+}
